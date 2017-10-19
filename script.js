8f4ab9b96b00f56cada202327a236dc736672d87
maudlareine <32840066+maudlareine@users.noreply.github.com>
2017-10-19 16:17:04 +0200

    J4
    améliorations commentaires

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $(".TOPLIST").click(function(){
-        $(".carousel").toggle(10000);
+        $(".carousel").toggle(500);
     });
 
 $.getJSON("https://api.airtable.com/v0/appwd6yCHy68SJWZz/musics?api_key=keyhZGarU6yaFz3CW",
@@ -10,6 +10,7 @@ $.getJSON("https://api.airtable.com/v0/appwd6yCHy68SJWZz/musics?api_key=keyhZGar
             $.each(data.records, function(key,val) {
               console.log(val.fields.title);
               $('<li>'+val.fields.title+'</li>').appendTo('#TOPLIST');
+              $('<li>'+val.fields.star+'</li>').appendTo('#TOPLIST');
               $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#TOPLIST');
 
             });
